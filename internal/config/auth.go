ddd9215f1b30a95234030c915b8a1a2b1d41bf0e
Dmitry_Kutsevalov <kutdmitry@gmail.com>
2025-12-06 12:33:37 +0300

    увеличение сессий до 10

--- internal/config/auth.go
+++ internal/config/auth.go
@@ -14,7 +14,7 @@ type authConfig struct {
 }
 
 func NewAuthConfig() AuthConfig {
-	maxSessions := 5 // default
+	maxSessions := 10
 	if maxSessionsStr := os.Getenv(maxSessionsEnvName); maxSessionsStr != "" {
 		if parsed, err := strconv.Atoi(maxSessionsStr); err == nil && parsed > 0 {
 			maxSessions = parsed
