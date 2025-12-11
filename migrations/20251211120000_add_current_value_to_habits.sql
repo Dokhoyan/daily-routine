0e97902ef1e7a783ab240495645b0a614e498c80
Dmitry_Kutsevalov <kutdmitry@gmail.com>
2025-12-11 14:48:37 +0300

    еще одна миграция в бд

--- migrations/20251211120000_add_current_value_to_habits.sql
+++ migrations/20251211120000_add_current_value_to_habits.sql
@@ -1,8 +1,15 @@
--- Add current_value column for partial habit completion
+-- +goose Up
+-- +goose StatementBegin
 ALTER TABLE habits
     ADD COLUMN IF NOT EXISTS current_value INTEGER DEFAULT 0 CHECK (current_value <= value);
 
--- Backfill existing rows to a safe value
 UPDATE habits
 SET current_value = LEAST(COALESCE(current_value, 0), value)
 WHERE current_value IS NULL;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE habits
+    DROP COLUMN IF EXISTS current_value;
+-- +goose StatementEnd
